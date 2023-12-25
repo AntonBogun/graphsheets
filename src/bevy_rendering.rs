003fe724f731e69902118d6162e73b0de8f3a53e
Danii06 <daanniiionline+github@gmail.com>
2023-12-25 17:07:18 +0200

    Revert "Fixed bevy app hanging"
    This reverts commit 8a38113138ba6a51ed99a816eb204e187b61ee42.

--- src/bevy_rendering.rs
+++ src/bevy_rendering.rs
@@ -12,48 +12,30 @@ pub fn start(graph: ge::GraphRef){
 
 
 fn setup(mut commands: Commands, graph: Res<Graph>){
-	println!("3");
-	
 	commands.spawn(Camera2dBundle::default());
 	graph.0.lock().unwrap().nodes.iter().for_each(|node| {
-		let node_lock = node.lock().unwrap();
-	println!("7");
-
 		commands.spawn(NodeBundle {
 			node: NodeComponent(node.clone()),
-
 			sprite: SpriteBundle {
 				sprite: Sprite {
 					color: Color::rgb(0.0, 0.0, 1.0),
 					custom_size: Some(Vec2::new(100.0, 100.0)),
 					..default()
 				},
-				transform: Transform::from_xyz(node_lock.position.0 as f32, node_lock.position.1 as f32,0.0),
+				transform: Transform::from_xyz(node.lock().unwrap().position.0 as f32, node.lock().unwrap().position.1 as f32,0.0),
 				..default()
 			},
 		});
 	});
-	println!("4");
-
 	graph.0.lock().unwrap().edges.iter().for_each(|edge| {
-		let edge_lock = edge.lock().unwrap();	
-		let edge_from = edge_lock.get_from();
-		let edge_to = edge_lock.get_to();
-		let edge_lock_from = edge_from.lock().unwrap();
-		let edge_lock_to = edge_to.lock().unwrap();
-		let from =Vec2::new(edge_lock_from.position.0 as f32,edge_lock_from.position.1 as f32);  
-		let to = Vec2::new(edge_lock_to.position.0 as f32,edge_lock_to.position.1 as f32);
-
 		commands.spawn(EdgeBundle {
-
 			edge: EdgeComponent(edge.clone()),
 			line: Line {
-				line: (from, to)
+				line: ((Vec2::new(edge.lock().unwrap().get_from().lock().unwrap().position.0 as f32,edge.lock().unwrap().get_from().lock().unwrap().position.1 as f32)), 
+				(Vec2::new(edge.lock().unwrap().get_to().lock().unwrap().position.0 as f32,edge.lock().unwrap().get_to().lock().unwrap().position.1 as f32)))
 			},
 		});
 	});
-	println!("5");
-	
 }
 
 #[derive(Resource)]
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,14 @@ use GraphElements as ge;
 mod bevy_rendering;
 use bevy_rendering as br;
  fn main(){
-	println!("0");
-
 	let mut graph = ge::Graph::new();
 	let grid1 = ge::Grid{ id: 0, cells: vec![]};
-	let node1 = ge::Node::new("node1".to_string(), (150.0,0.0), 10.0 ,Arc::new(Mutex::new(grid1)));
+	let node1 = ge::Node::new("node1".to_string(), (0.0,0.0), 10.0 ,Arc::new(Mutex::new(grid1)));
 	graph.lock().unwrap().add_node(node1.clone());
-	println!("1");
 	let grid2 = ge::Grid{ id: 0, cells: vec![]};
-	let node2 = ge::Node::new("node2".to_string(), (0.0,150.0), 10.0 ,Arc::new(Mutex::new(grid2)));
+	let node2 = ge::Node::new("node2".to_string(), (0.0,0.0), 10.0 ,Arc::new(Mutex::new(grid2)));
 	graph.lock().unwrap().add_node(node2.clone());
 	let edge1 = ge::Edge::new(node1.clone(), node2.clone(), "edge1".to_string());
 	graph.lock().unwrap().add_edge(edge1);
-	println!("2");
 	br::start(graph);
  } 
